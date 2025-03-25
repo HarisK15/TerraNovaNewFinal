0e1c067eae5101113745ad5b3429690be0ab5918
haris kamran <harisk543@hotmail.com>
2025-03-25 17:23:45 +0000

    QueryPage

--- frontend/src/utils/formatIcons.js
+++ frontend/src/utils/formatIcons.js
@@ -3,23 +3,19 @@ import DescriptionIcon from '@mui/icons-material/Description';
 import TableViewIcon from '@mui/icons-material/TableView';
 import CodeIcon from '@mui/icons-material/Code';
 
-// This function returns the right icon for each export format
+//returns icons based on format
 export const getFormatIcon = (format) => {
-  // Convert to lowercase just in case
+  // convert to lowercase just in case
   const formatLower = format.toLowerCase();
   
-  // Check which format we have
   if (formatLower === 'csv') {
-    // CSV gets a document icon
     return <DescriptionIcon />;
   } else if (formatLower === 'excel') {
-    // Excel gets a table icon
     return <TableViewIcon />;
   } else if (formatLower === 'json') {
-    // JSON gets a code icon
     return <CodeIcon />;
   } else {
-    // If we don't recognize the format, default to document icon
+    //default to document icon
     console.log('Unknown format:', format);
     return <DescriptionIcon />;
   }
